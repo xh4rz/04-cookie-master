9264b8ee7fbf601b6257b2ee46ef54d6a5ed59f4
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-08-30 23:21:49 -0500

    163. Página para cambiar el tema

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,9 +1,45 @@
+import { ChangeEvent, useState } from 'react';
+import {
+	Card,
+	CardContent,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	Radio,
+	RadioGroup
+} from '@mui/material';
 import { Layout } from '../components/layouts';
 
 const ThemeChangerPage = () => {
+	const [currentTheme, setCurrentTheme] = useState('light');
+
+	const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const selectedTheme = event.target.value;
+		setCurrentTheme(selectedTheme);
+	};
+
 	return (
 		<Layout>
-			<h1>ThemeChangerPage</h1>
+			<Card>
+				<CardContent>
+					<FormControl>
+						<FormLabel>Tema</FormLabel>
+						<RadioGroup value={currentTheme} onChange={onThemeChange}>
+							<FormControlLabel
+								value="light"
+								control={<Radio />}
+								label="Light"
+							/>
+							<FormControlLabel value="dark" control={<Radio />} label="Dark" />
+							<FormControlLabel
+								value="custom"
+								control={<Radio />}
+								label="Custom"
+							/>
+						</RadioGroup>
+					</FormControl>
+				</CardContent>
+			</Card>
 		</Layout>
 	);
 };
