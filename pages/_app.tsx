c249b9fff0c75291f6a6a82dd646c4f83641ac54
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-08-31 23:28:17 -0500

    168. Generar la página con el tema solicitado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,34 @@
 import '../styles/globals.css';
 import type { AppContext, AppProps } from 'next/app';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material';
 import { darkTheme, lightTheme, customTheme } from '../themes';
 
-function MyApp({ Component, pageProps, ...rest }: AppProps) {
-	console.log({ rest });
+interface Props extends AppProps {
+	theme: string;
+}
+
+function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
+	const currentTheme: Theme =
+		theme === 'light' ? lightTheme : theme === 'dark' ? darkTheme : customTheme;
 
 	return (
-		<ThemeProvider theme={darkTheme}>
+		<ThemeProvider theme={currentTheme}>
 			<CssBaseline />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
 }
 
-MyApp.getInitialProps = async (appContext: AppContext) => {
-	const { theme } = appContext.ctx.req
-		? (appContext.ctx.req as any).cookies
-		: { theme: 'light' };
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+// 	const { theme } = appContext.ctx.req
+// 		? (appContext.ctx.req as any).cookies
+// 		: { theme: 'light' };
 
-	const validThemes = ['light', 'dark', 'custom'];
+// 	const validThemes = ['light', 'dark', 'custom'];
 
-	return {
-		theme: validThemes.includes(theme) ? theme : 'dark'
-	};
-};
+// 	return {
+// 		theme: validThemes.includes(theme) ? theme : 'dark'
+// 	};
+// };
 
 export default MyApp;
